e52d30fa8ceb4e2981054621d68d518e3e267bd3
David Von Lehman <david.vonlehman@gmail.com>
2015-10-26 16:25:17 -0700

    Support for data-src-keep custom attribute to prevent rewriting src attributes to absolute URLs. Also fixing self-closing tag behavior.

--- test/htmlprep.js
+++ test/htmlprep.js
@@ -20,7 +20,7 @@ describe('htmlprep()', function() {
 
     runProcessor(html, function(err, output) {
       if (err) return done(err);
-      assert.equal(output, html);
+      assert.equal(output, '<html><head><link rel="stylesheet" href="css/styles.css"/></head></html>');
       done();
     });
   });
@@ -66,7 +66,7 @@ describe('htmlprep()', function() {
     runProcessor(html, options, function(err, output) {
       if (err) return done(err);
 
-      assert.equal(output, '<html><img src="release.jpg"></html>');
+      assert.equal(output, '<html><img src="release.jpg"/></html>');
       done();
     });
   });
@@ -149,7 +149,7 @@ describe('htmlprep()', function() {
     runProcessor(html, options, function(err, output) {
       if (err) return done(err);
 
-      assert.equal(output, '<html><body><div><!-- block1 --></div><hr><div><!-- block2 --></div></body></html>');
+      assert.equal(output, '<html><body><div><!-- block1 --></div><hr/><div><!-- block2 --></div></body></html>');
       done();
     });
   });
@@ -160,7 +160,7 @@ describe('htmlprep()', function() {
     runProcessor(html, {assetPathPrefix: '//cdnhost/abcd'}, function(err, output) {
       if (err) return done(err);
 
-      assert.equal(output, '<html><head><title>title</title><link rel="stylesheet" href="//cdnhost/abcd/css/styles.css"></head></html>');
+      assert.equal(output, '<html><head><title>title</title><link rel="stylesheet" href="//cdnhost/abcd/css/styles.css"/></head></html>');
       done();
     });
   });
@@ -171,7 +171,7 @@ describe('htmlprep()', function() {
     runProcessor(html, {assetPathPrefix: '//cdnhost/abc'}, function(err, output) {
       if (err) return done(err);
 
-      assert.equal(output, '<html><body><script src="//cdnhost/abc/js/app.js"></script><img src="//cdnhost/abc/images/logo.jpg"></body></html>');
+      assert.equal(output, '<html><body><script src="//cdnhost/abc/js/app.js"></script><img src="//cdnhost/abc/images/logo.jpg"/></body></html>');
       done();
     });
   });
@@ -194,7 +194,7 @@ describe('htmlprep()', function() {
 
       // assert.equal(output, '<html><link rel="stylesheet" href="//cdn.com/css/styles.css"></html>');
 
-      assert.equal(output, '<head><link rel="stylesheet" href="//cdn.com/bower_components/bootstrap.css"><link rel="stylesheet" href="//cdn.com/tmp/styles.css"></head>');
+      assert.equal(output, '<head><link rel="stylesheet" href="//cdn.com/bower_components/bootstrap.css"/><link rel="stylesheet" href="//cdn.com/tmp/styles.css"/></head>');
       done();
     });
   });
@@ -216,7 +216,7 @@ describe('htmlprep()', function() {
     runProcessor(html, function(err, output) {
       if (err) return done(err);
 
-      assert.equal(output, '<html><head><link rel="stylesheet" href="app.css"></head></html>');
+      assert.equal(output, '<html><head><link rel="stylesheet" href="app.css"/></head></html>');
       done();
     });
   });
@@ -297,6 +297,17 @@ describe('htmlprep()', function() {
   it('preserves chinese characters', function(done) {
     var html = '<p>我通常使用Socks或者Http代理做为科学上网方案， 但仍然有很多需要全局代理的场景， 例如像Android SDK类似的各种墙外工具的更新， 或者不能使用Socks代理的手机等， 还有的时候我们希望为亲朋好友提供一个科学上网的方案， 只需要账号密码的VPN方式是最佳的选择。 如果你需要你的VPN更加安全稳定， 那我要推荐你使用OpenVPN和ShadowVPN， 他们更加高效和安全。 当然如果你使用个人的VPS做PPTP VPN， 被追踪的可能也很小， 最重要的是如果你需要这个VPN不依赖于客户端随时随地任意设备可用， PPTP VPN最佳选择之一。 阅读以下内容前，您应该拥有一个可访问的VPS(什么是VPS？)。 可选的VPS有很多，热门的有DigitalOcean, Linode, Vultr, Bandwagon(俗称‘搬瓦工’)等等。 我个人推荐Vultr和DigitalOcean，理由是便宜、稳定、SSD、机房多、有日本机房(中国访问快)， 最近维护VPS的时候出现了一些问题， 客服态度非常好， 反应也很迅速， 这个非常重要。 使用以下链接注册可以帮你立省10$， 够用两个月了。 这也是我写这篇文章的动力之一， 这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，这也是我写这篇文章的动力之一，你好我好大家好： http://www.vultr.com/?ref ...</p>';
     runProcessor(html, function(err, output) {
+      if (err) return done(err);
+
+      assert.equal(output, html);
+      done();
+    });
+  });
+
+  it('does not rewrite URLs of images with data-src-keep attribute', function(done) {
+    var html = '<html><img src="/media/logo.png" data-src-keep/></html>';
+
+    runProcessor(html, {assetPathPrefix: 'cdnhost.com'}, function(err, output) {
       if (err) return done(err);
 
       assert.equal(output, html);
